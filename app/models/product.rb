cb87b0bcc48e94da873099f903839e1109a55980
dogaruemiliano <dogaru.emiliano@gmail.com>
2022-04-09 19:03:57 +0200

    Added validations for Product (all tests are succesfull)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :product_code, presence: true, length: { minimum: 3 }
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
