7a4c823541824821916e8900ec14276b09f36d7a
dogaruemiliano <dogaru.emiliano@gmail.com>
2022-04-09 19:53:54 +0200

    Added RSpec test for Basket and BasketItem (they are related)

--- spec/models/basket_item_spec.rb
+++ spec/models/basket_item_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe BasketItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    product = Product.create!(product_code: 'SR1', name: 'Strawberry', price: 5.00)
+    basket = Basket.create!
+    described_class.create!(product: product, basket: basket, amount: 3)
+  end
+  
+  describe 'Association' do
+    it { should belong_to :basket }
+    it { should belong_to :product }
+  end
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+    it 'is not valid without a product' do
+      subject.product = nil
+      expect(subject).to_not be_valid
+    end
+    it 'is not valid without basket' do
+      subject.basket = nil
+      expect(subject).to_not be_valid
+    end
+    it 'is not valid without an amount' do
+      subject.amount = nil
+      expect(subject).to_not be_valid
+    end
+    it 'is not valid with a neutral amount' do
+      subject.amount = 0
+      expect(subject).to_not be_valid
+    end
+    it 'is not valid with a negative amount' do
+      subject.amount = -2
+      expect(subject).to_not be_valid
+    end
+
+    describe "Methods" do
+      it 'should return the total cost of products of this type' do
+        expect(subject.total_cost).to eq(15)
+      end
+    end
+    
+  end
 end
--- spec/models/basket_spec.rb
+++ spec/models/basket_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Basket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    p1 = Product.create!(product_code: 'GR1', name: 'Green Tea', price: 3.11)
+    p2 = Product.create!(product_code: 'SR1', name: 'Strawberry', price: 5.00)
+    p3 = Product.create!(product_code: 'CF1', name: 'Coffe', price: 11.23)
+
+    sub = described_class.create!
+  
+    BasketItem.create(product: p1, basket: sub, amount: 1)
+    BasketItem.create(product: p2, basket: sub, amount: 5)
+    BasketItem.create(product: p3, basket: sub, amount: 8)
+    
+    sub
+  end
+
+  describe "Associations" do
+    it { should have_many :basket_items}
+  end
+
+  describe 'Methods' do
+    it 'shows the total price of the basket' do
+      expect(subject.total_cost).to eq(117.95)
+    end
+  end
 end
