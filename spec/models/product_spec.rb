78d12ca42cc959bb30029e44d0e699ec9f0539c8
dogaruemiliano <dogaru.emiliano@gmail.com>
2022-04-09 18:59:16 +0200

    ?

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,40 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  subject do
-    described_class.new(product_code: 'SR1', name: 'Strawberry', price: 5.00)
-  end
-  describe 'Validations' do
-    it 'is valid with valid attributes' do
-      expect(subject).to be_valid
-    end
-    it 'it is not valid without a product code' do
-      subject.product_code = nil
-      expect(subject).not_to be_valid
-    end
-    it 'it is not valid with a product code shorter than 3 chars' do
-      subject.product_code = 'GR'
-      expect(subject).not_to be_valid
-    end
-    it 'it is not valid without a name' do
-      subject.name = nil
-      expect(subject).not_to be_valid
-    end
-    it 'it is not valid with a name shorter than 3 chars' do
-      subject.name = 'Ab'
-      expect(subject).not_to be_valid
-    end
-    it 'it is not valid without a price' do
-      subject.price = nil
-      expect(subject).not_to be_valid
-    end
-    it 'it is not valid with a neutral price' do
-      subject.price = 0
-      expect(subject).not_to be_valid
-    end
-    it 'it is not valid with a negative price' do
-      subject.price = -1
-      expect(subject).not_to be_valid
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
