71172349425ad10d24119f03b639bbb5a811ac95
dogaruemiliano <dogaru.emiliano@gmail.com>
2022-04-09 20:04:43 +0200

    Added validations and created total cost methods for Basket and BasketItem (all tests are successfull)

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,2 +1,11 @@
 class Basket < ApplicationRecord
+  has_many :basket_items
+
+  def total_cost
+    cost = 0
+    basket_items.each do |item|
+      cost += item.total_cost
+    end
+    cost
+  end
 end
--- app/models/basket_item.rb
+++ app/models/basket_item.rb
@@ -1,4 +1,9 @@
 class BasketItem < ApplicationRecord
   belongs_to :product
   belongs_to :basket
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+
+  def total_cost
+   product.price * quantity
+  end
 end
